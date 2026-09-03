f44447088da882c6892c30b6c183dfd0e34de0b3
Hugo Arevalo <gohulu@gmail.com>
2026-09-02 20:48:23 -0500

    Initial commit

---
+++ src/fastapi_fundamentals/schemas.py
@@ -0,0 +1,46 @@
+import json
+from datetime import datetime
+from pathlib import Path
+
+from sqlmodel import Relationship, SQLModel, Field
+
+
+class TripInput(SQLModel):
+    start: datetime
+    end: datetime
+    distance: int
+
+
+class TripOutput(TripInput):
+    id: int
+
+
+class Trip(TripInput, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    car_id: int = Field(foreign_key="car.id")
+    car: "Car" = Relationship(back_populates="trips")
+
+
+class CarInput(SQLModel):
+    size: str
+    fuel: str | None = "electric"
+    doors: int
+    transmission: str | None = "auto"
+
+    model_config = {
+        "json_schema_extra": {
+            "examples": [
+                {"size": "m", "doors": 5, "transmission": "manual", "fuel": "hybrid"}
+            ]
+        }
+    }
+
+
+class Car(CarInput, table=True):
+    id: int | None = Field(primary_key=True, default=None)
+    trips: list[Trip] = Relationship(back_populates="car")
+
+
+class CarOutput(CarInput):
+    id: int
+    trips: list[TripOutput] = []
